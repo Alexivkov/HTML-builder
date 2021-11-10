5367bc2e1aa29533ece3e1cba69f6bf0b83d15b9
Alexander Sivkov <sivkov@inbox.ru>
2021-11-10 21:27:18 +0300

    fix: work for changing files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,31 +1,52 @@
 const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
-const pathDirectoryCopy = path.join(__dirname, 'files-copy');
-//это НЕ экспериментальная функция fsPromises.cp() пункт 2 проверки!!!
-fsPromises.mkdir(pathDirectoryCopy).then(function() {
-  console.log('Directory created successfully');
-}).catch(function() {
-  fs.readdir(pathDirectoryCopy, (err, files) => {
-    if (err) throw err;
-    for (const file of files) {
-      fs.unlink(path.join(__dirname, 'files-copy' , file), err => {
-        if (err) throw err;
-      });
-    }
-  });
-  console.log('failed to create directory');
-});
+const FILES_DIR = 'files'
+const FILES_COPY_DIR = 'files-copy'
 
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-  files.forEach(file => {
-    const filePath = path.join(__dirname, 'files', file);
-    const filePathCopy = path.join(__dirname, 'files-copy', file);
-    fs.copyFile(filePath, filePathCopy, (err) => {
-      if (err) throw err;
-      console.log('File Copy Successfully.');
-    });
-  });
-});
-    
+const branch1 = () => {
+  findDiff()
+    .then(deleteDifference)
+    .then(copyFiles)
+    .catch(err => "diff failed")
+}
+
+const branch2 = () =>
+  fsPromises.mkdir(path.join(__dirname, FILES_COPY_DIR))
+    .then(copyFiles)
+
+fsPromises.stat(path.join(__dirname,FILES_COPY_DIR))
+.then(branch1)
+.catch(branch2)
+
+function copyFiles(files = []) {
+  if (files.length < 1) {
+    return fsPromises.readdir(path.join(__dirname,FILES_DIR)).then(files => {
+      copyFileResolve(files)
+    })
+  } else {
+    copyFileResolve(files)
+  }
+}
+
+function deleteDifference({ filesToDelete, sourceFiles }) {
+  return Promise.all(filesToDelete.map(f => fsPromises.unlink(path.join(__dirname,FILES_COPY_DIR, f))))
+    .then(() => sourceFiles)
+}
+
+
+function findDiff(/* stat */) {
+  const targetFilesPromise = fsPromises.readdir(path.join(__dirname,FILES_COPY_DIR))
+  const sourceFilesPromise = fsPromises.readdir(path.join(__dirname,FILES_DIR))
+  return Promise.all([targetFilesPromise, sourceFilesPromise])
+    .then(([targetFiles, sourceFiles]) => {
+      const filesToDelete = targetFiles.filter(x => sourceFiles.indexOf(x) < 0)
+      return { filesToDelete, sourceFiles }
+    })
+}
+
+function copyFileResolve(files) {
+  return Promise.all(files.map(f =>
+    fsPromises.copyFile(path.join(__dirname,FILES_DIR, f), path.join(__dirname,FILES_COPY_DIR, f))))
+}
 
